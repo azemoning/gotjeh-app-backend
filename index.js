2fb1fe3b809847bddd6d0f0e141560fad152ada9
Prayogo Dewantoro <pygdwt@gmail.com>
2021-03-04 15:05:01 +0800

    [Yogo] - Added middlewares and remove restrict for testing purposes

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routers/indexRoute"));
 app.use("/auth", require("./routers/authRoute"));
-app.use("/user", restrict, require("./routers/userRoute"));
-app.use("/jobs", restrict, require("./routers/jobsRoute"));
-app.use("/subscriptions", restrict, require("./routers/subscriptionsRoute"));
-app.use("/categories", restrict, require("./routers/categoriesRoute"));
+app.use("/user", require("./routers/userRoute"));
+app.use("/jobs", require("./routers/jobsRoute"));
+app.use("/subscriptions", require("./routers/subscriptionsRoute"));
+app.use("/categories", require("./routers/categoriesRoute"));
 
 app.use(function (error, req, res, next) {
   res.send(error);
