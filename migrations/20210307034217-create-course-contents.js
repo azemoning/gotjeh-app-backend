22b1811bbdb03fdeec30ecce32bf8124c68b723d
azemoning <ahmadsaufimaulana@gmail.com>
2021-03-13 00:28:09 +0700

    [hotfix] readd migration file

---
+++ migrations/20210307034217-create-course-contents.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Course_Contents', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Course_Contents');
+  }
+};
