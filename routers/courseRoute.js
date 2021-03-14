d5d1afbf186ea108507bc0a9a34daec94746acb5
azemoning <ahmadsaufimaulana@gmail.com>
2021-03-14 22:09:24 +0700

    [hotfix] fix middlewares on some routes

--- routers/courseRoute.js
+++ routers/courseRoute.js
@@ -1,12 +1,12 @@
 const app = require("express").Router();
 const CourseController = require("../controllers/courseController");
-const isExist = require("../middlewares/isExist");
-const { Courses, Course_Sections, Course_Contents } = require("../models");
 const course = new CourseController();
+const passport = require("../middlewares/passportMiddleware");
+const restrict = passport.authenticate('jwt', { session: false })
 
 app.route("/").get(course.getAllCourses()).post(course.addNewCourse());
 app.route("/search").get(course.searchByCourseTitle());
-app.route("/enrolled").get(course.getEnrolledCourses())
+app.route(restrict, "/enrolled").get(course.getEnrolledCourses())
 app
   .route("/filter")
   .get(course.filterByCourseCategory());
@@ -16,17 +16,17 @@ app.route("/sections").post(course.addNewCourseSection());
 
 app
   .route("/sections/:id")
-  .get(isExist(Course_Sections), course.getCourseSectionById())
-  .put(isExist(Course_Sections), course.updateCourseSection())
-  .delete(isExist(Course_Sections), course.deleteCourseSection());
+  .get(course.getCourseSectionById())
+  .put(course.updateCourseSection())
+  .delete(course.deleteCourseSection());
 
 app.route("/popular").get(course.filterByPopularCourse());
 
 
 app
   .route("/:id")
-  .get(isExist(Courses), course.getCourseById())
-  .put(isExist(Courses), course.updateCourse())
-  .delete(isExist(Courses), course.deleteCourse());
+  .get(course.getCourseById())
+  .put(course.updateCourse())
+  .delete(course.deleteCourse());
 
 module.exports = app;
